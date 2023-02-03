8baa39c412ef1913db49a69e01b6438fdaedd577
MStryapchiy <MustStr@gmail.com>
2023-02-03 13:48:12 +0400

    add 2.1 and 2.2 solutions

---
+++ 2_1.py
@@ -0,0 +1,25 @@
+# 2.1
+
+# n=10
+# sum=0
+# while n!=0:
+#     n=int(input())
+#     sum = sum+n
+# print(sum)
+
+# a = int(input())
+# b = int(input())
+# n=1
+# while (n%a!=0 or n%b!=0):
+#     n+=1
+# print(n)
+
+# 2.2
+# a=0
+# while (a<=100):
+#     a = int(input())
+#     if a<10:
+#         continue
+#     if a>100:
+#         break
+#     print(a)
